19f00aa1428074e0e7608ff5b7aa21b4fa0f68a0
matthiaskozubal <mkoz001@gmail.com>
2021-08-13 18:30:43 +0200

    Add files via upload
    Version 2, use function return_coordinates(place):
    
        Returns coordinates of the place.
        The place must be the name (in polish) of voivodeship in Poland or the district of Warsaw.
        E.g.:
          1)
            In:  return_coordinates('województwo podlaskie')
            Out: [53.267219444444, 22.931938888889]
          2) 
            In:  return_coordinates('podlaskie')
            Out: [53.267219444444, 22.931938888889]
          3)
            In:  return_coordinates('PODLASKIE')
            Out: [53.267219444444, 22.931938888889]
          4) 
            In:  return_coordinates('Ursus')
            Out: [52.19517, 20.88419]

--- poland_geographical_locations.py
+++ poland_geographical_locations.py
@@ -1,66 +1,26 @@
-'''
+# Info
 * https://www.wikidata.org/wiki/Wikidata:SPARQL_tutorial
 * https://query.wikidata.org/
 * https://janakiev.com/blog/wikidata-mayors/
-
-Example call-outs:
-  * coordinates_of_voivodeship('województwo podlaskie') -> [53.267219444444, 22.931938888889]
-  * coordinates_of_voivodeship('podlaskie')             -> [53.267219444444, 22.931938888889]
-'''
+* https://en.wikibooks.org/wiki/SPARQL/WIKIDATA_Precision,_Units_and_Coordinates#Coordinates
+* https://www.wikidata.org/wiki/Wikidata:SPARQL_query_service/queries/examples#Mountains
 
 
-# Libraries
+# Imports
 import requests
 import pandas as pd
 from collections import OrderedDict
+import numpy as np
 
 
-# Old function
-def get_voivodeships_v1():
-    # problem - only ten voivodeships has the inception date se
-    # solution - version 2 of this function
-    
-    # request voivodeships of poland(wd:Q36)
-    # https://query.wikidata.org/#%20%20%20%20SELECT%20%3Fvoivodeship%20%3FvoivodeshipLabel%20%3Finception%20%3Flocation%0A%20%20%20%20WHERE%20%7B%0A%20%20%20%20%20%20%3Fvoivodeship%20wdt%3AP31%20wd%3AQ150093%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20wdt%3AP571%20%3Finception%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20wdt%3AP625%20%3Flocation.%0A%20%20%20%20%20%20FILTER%28%3Finception%20%3E%3D%20%221999-01-01%22%5E%5Exsd%3AdateTime%29.%0A%0A%20%20%20%20%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%2Cen%22.%20%7D%0A%20%20%20%20%7D
-    url = 'https://query.wikidata.org/sparql'
-    query = '''
-    SELECT ?voivodeship ?voivodeshipLabel ?inception ?location
-    WHERE {
-      ?voivodeship wdt:P31 wd:Q150093;
-                   wdt:P571 ?inception;
-                   wdt:P625 ?location.
-      FILTER(?inception >= "1999-01-01"^^xsd:dateTime).
-
-      SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-    }
-    '''
-    r = requests.get(url, params = {'format': 'json', 'query': query})
-    data = r.json()
-
-    # convert json to dataframe
-    voivodeships = []
-    for item in data['results']['bindings']:
-        voivodeships.append(OrderedDict(
-        {
-            'voivodeship':      item['voivodeshipLabel']['value'],
-            'location':         item['location']['value'],        
-            'wikidata_item_id': item['voivodeship']['value'].split('/')[-1]
-        }))
-    #     print(item,'\n')
-
-    df = pd.DataFrame(voivodeships)
-    return df
-
-
-# Correct function
-def get_voivodeships_v2():
+# Defs
+def get_voivodeships():
     # problem - too many voivodeships, historical ones are mixed together
     # solution - just cut the list after 16-th item
     
-    # request voivodeships of poland(wd:Q36)
+    # request voivodeships of poland(wd:Q36),
     # cut after 16 voivodeships, the rest are historical ones
-    # https://query.wikidata.org/#SELECT%20%3Fvoivodeship%20%3FvoivodeshipLabel%20%3Flatitude%20%3Flongitude%20%3Fadmininistrative_teritorial_entity%0AWHERE%20%7B%0A%20%20%3Fvoivodeship%20wdt%3AP31%20wd%3AQ150093%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20wdt%3AP131%20%3Fadmininistrative_teritorial_entity%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20p%3AP625%2Fpsv%3AP625%20%5B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20wikibase%3AgeoLatitude%20%3Flatitude%20%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20wikibase%3AgeoLongitude%20%3Flongitude%20%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%5D.%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%0A%20%20FILTER%28%3Fadmininistrative_teritorial_entity%20%3D%20wd%3AQ36%29.%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22pl%22.%20%7D%0A%7D%0AORDER%20BY%20DESC%28%3Fvoivodeship%29%0ALIMIT%2016
-    # https://www.wikidata.org/wiki/Wikidata:SPARQL_query_service/queries/examples#Mountains
+    # https://query.wikidata.org/#%20%20%20%20SELECT%20%3Fvoivodeship%20%3FvoivodeshipLabel%20%3Flatitude%20%3Flongitude%20%3Fadmininistrative_teritorial_entity%0A%20%20%20%20WHERE%20%7B%0A%20%20%20%20%20%20%3Fvoivodeship%20wdt%3AP31%20wd%3AQ150093%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20wdt%3AP131%20%3Fadmininistrative_teritorial_entity%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20p%3AP625%2Fpsv%3AP625%20%5B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20wikibase%3AgeoLatitude%20%3Flatitude%20%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20wikibase%3AgeoLongitude%20%3Flongitude%20%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%5D.%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20FILTER%28%3Fadmininistrative_teritorial_entity%20%3D%20wd%3AQ36%29.%0A%20%20%20%20%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22pl%22.%20%7D%0A%20%20%20%20%7D%0A%20%20%20%20ORDER%20BY%20DESC%28%3Fvoivodeship%29%0A%20%20%20%20LIMIT%2016%0A
     url = 'https://query.wikidata.org/sparql'
     query = '''
     SELECT ?voivodeship ?voivodeshipLabel ?latitude ?longitude ?admininistrative_teritorial_entity
@@ -85,9 +45,9 @@ def get_voivodeships_v2():
     for item in data['results']['bindings']:
         voivodeships.append(OrderedDict(
         {
-            'voivodeship':      item['voivodeshipLabel']['value'],
+            'voivodeship':      item['voivodeshipLabel']['value'].lower(),
             'latitude':         float(item['latitude']['value']),        
-            'longitude':         float(item['longitude']['value']),                    
+            'longitude':        float(item['longitude']['value']),                    
             'wikidata_item_id': item['voivodeship']['value'].split('/')[-1]
         }))
     #     print(item,'\n')    
@@ -95,8 +55,98 @@ def get_voivodeships_v2():
     df = pd.DataFrame(voivodeships)
     return df
 
+voivodeships = get_voivodeships()
+voivodeships
 
-def coordinates_of_voivodeship(wojewodztwo):
-    voivodeships = get_voivodeships_v2()
-    coordinates = voivodeships.loc[voivodeships['voivodeship'].str.contains(wojewodztwo) == True, ['latitude', 'longitude']]
-    return coordinates.values.tolist()[0]
+
+def get_warsaw_districts():
+    # problem  - some districts have two sets of coordinates which differ slighlty
+    # solution - drop the one with worse precision (larger value), this seems to be consistent with the wikipedia data
+    # to do    - the above is not true for ['Wola'], change it
+    
+    # https://query.wikidata.org/#SELECT%20%3Fdistrict_of_Warsaw%20%3Fdistrict_of_WarsawLabel%20%3Flat%20%3Flon%20%3FgeoPrecision%20%0AWHERE%20%7B%0A%20%20%3Fdistrict_of_Warsaw%20%20wdt%3AP31%20wd%3AQ4286337%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20wdt%3AP17%20wd%3AQ36%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20wdt%3AP131%20wd%3AQ270%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20p%3AP625%2Fpsv%3AP625%20%5B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20wikibase%3AgeoLatitude%20%3Flat%20%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20wikibase%3AgeoLongitude%20%3Flon%20%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20wikibase%3AgeoPrecision%20%20%3FgeoPrecision%3B%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%5D%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22pl%22.%20%7D%0A%7D
+    url = 'https://query.wikidata.org/sparql'
+    query = '''
+    SELECT ?warsaw_district ?warsaw_districtLabel ?latitude ?longitude ?geoPrecision 
+    WHERE {
+      ?warsaw_district  wdt:P31 wd:Q4286337;
+                           wdt:P17 wd:Q36;
+                           wdt:P131 wd:Q270;
+                           p:P625/psv:P625 [
+                               wikibase:geoLatitude ?latitude ;
+                               wikibase:geoLongitude ?longitude ;
+                               wikibase:geoPrecision  ?geoPrecision;                                       
+                           ]
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "pl". }
+    }
+    ORDER BY ASC(?warsaw_districtLabel)
+    '''
+    r = requests.get(url, params = {'format': 'json', 'query': query})
+#     print(r.json())
+    data = r.json()
+
+    # convert json to dataframe
+    warsaw_districts = []
+    for item in data['results']['bindings']:
+#         print(item,'\n')    
+        warsaw_districts.append(OrderedDict(
+        {
+            'warsaw_district':  item['warsaw_districtLabel']['value'].lower(),
+            'latitude':         float(item['latitude']['value']),        
+            'longitude':        float(item['longitude']['value']),                    
+            'geoPrecision':     float(item['geoPrecision']['value']),                                
+            'wikidata_item_id': item['warsaw_district']['value'].split('/')[-1]
+        }))
+    
+    warsaw_districts = pd.DataFrame(warsaw_districts).sort_values(by=['warsaw_district'])
+    
+    # if duplicate warsaw districts exist, take the one with better precision (lower value), do the opposite in case of 'Wola' 
+    for district in warsaw_districts['warsaw_district']:
+        if np.sum(warsaw_districts['warsaw_district'] == district) > 1: # duplicate district found
+            if district != 'wola':            
+                district_to_drop_idx = warsaw_districts.loc[warsaw_districts['warsaw_district'] == district, 'geoPrecision'].idxmax()
+            elif district == 'wola':
+                district_to_drop_idx = warsaw_districts.loc[warsaw_districts['warsaw_district'] == district, 'geoPrecision'].idxmin()
+#             print(district+':\t', district_to_drop_idx, '\n')
+            warsaw_districts = warsaw_districts.drop(district_to_drop_idx).reset_index(drop=True)  
+            
+    return warsaw_districts
+
+warsaw_districts = get_warsaw_districts()
+warsaw_districts
+
+
+def return_coordinates(place):
+    '''
+    Returns coordinates of the place.
+    The place must be the name (in polish) of voivodeship in Poland or the district of Warsaw.
+    E.g.:
+      1)
+        In:  return_coordinates('województwo podlaskie')
+        Out: [53.267219444444, 22.931938888889]
+      2) 
+        In:  return_coordinates('podlaskie')
+        Out: [53.267219444444, 22.931938888889]
+      3)
+        In:  return_coordinates('PODLASKIE')
+        Out: [53.267219444444, 22.931938888889]
+      4) 
+        In:  return_coordinates('Ursus')
+        Out: [52.19517, 20.88419] 
+    '''
+    
+    coordinates = [None, None]
+    place = place.lower()
+    
+    try:
+        voivodeships = get_voivodeships()
+        coordinates_df = voivodeships.loc[voivodeships['voivodeship'].str.contains(place) == True, ['latitude', 'longitude']]
+        coordinates = coordinates_df.values.tolist()[0]       
+    except IndexError:
+        try:
+            warsaw_districts = get_warsaw_districts()
+            coordinates_df = warsaw_districts.loc[warsaw_districts['warsaw_district'].str.contains(place) == True, ['latitude', 'longitude']]
+            coordinates = coordinates_df.values.tolist()[0]
+        except IndexError:
+            pass
+    return coordinates
