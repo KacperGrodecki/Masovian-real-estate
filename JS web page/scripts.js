03e388b31eebbd9972464e0c5b6061b369bfc476
mesonyktio <mkoz001@gmail.com>
2021-08-31 00:49:18 +0200

    NN online, parameters from user input, problem with loaded NN

--- JS web page/scripts.js
+++ JS web page/scripts.js
@@ -5,18 +5,27 @@
 // https://stackoverflow.com/questions/49432579/await-is-only-valid-in-async-function
 async function fetchNormalizationParameters() {
     // Load normalization parameters
-    const result = await fetch('https://raw.githubusercontent.com/matthiaskozubal/tests/0b15e0516d72f8a4e58adc8cd215de45b45448ef/normalization_parameters.json'); // https://github.com/matthiaskozubal/tests/blob/0b15e0516d72f8a4e58adc8cd215de45b45448ef/normalization_parameters.json
+    const result = await fetch('https://raw.githubusercontent.com/matthiaskozubal/tests/0b15e0516d72f8a4e58adc8cd215de45b45448ef/normalization_parameters.json');
     const NormalizationParameters = await result.json();
     // console.log('Normalization parameters inputMax:', NormalizationParameters.inputMax);
     return NormalizationParameters
 }
 
 
-// Manually declared normalization parameters
-// const inputMax = tf.tensor(230);
-const inputMin = tf.tensor(46);
-const labelMax = tf.tensor(46.599998474121094);
-const labelMin = tf.tensor(9);  
+// Newer function - returns means and variances
+async function fetchNormalizationParameters_new() {
+    var means = await fetch('https://raw.githubusercontent.com/KacperGrodecki/nieruchomosci-mazowieckie/0.0.4/JS%20web%20page/means.json');
+    means = await means.json();
+    means = tf.tensor(means[0]);//,shape=[means[0].length]);
+    // console.log('Normalization parameters, means:', means.shape);
+
+    var variances = await fetch('https://raw.githubusercontent.com/KacperGrodecki/nieruchomosci-mazowieckie/0.0.4/JS%20web%20page/variance.json'); // name will change after commit
+    variances = await variances.json();
+    variances = tf.tensor(variances[0]);
+    // console.log('Normalization parameters, variances:', variances.shape);
+
+    return {means, variances}
+}
 
 
 // Model loading
@@ -24,20 +33,19 @@ async function loadModel() {
         // https://support.mozilla.org/en-US/questions/1264280
         // change privacy_file_unique_origin to false in about:config
         // https://towardsdatascience.com/how-to-deploy-tensorflow-models-to-the-web-81da150f87f7
-        const loadedModel = await tf.loadLayersModel('https://raw.githubusercontent.com/matthiaskozubal/tests/main/cars_my_model.json');
+        const loadedModel = await tf.loadLayersModel('https://raw.githubusercontent.com/matthiaskozubal/tests/main/cars_my_model.json'); 
+        // const loadedModel = await tf.loadLayersModel('https://raw.githubusercontent.com/KacperGrodecki/nieruchomosci-mazowieckie/main/model.json');
         console.log('Model: ', loadedModel);
         return loadedModel;
     };
     
 
+// Taking user input parameters and returnig them as tensor
 function getUserInput() {
-    const userInput = document.getElementById('userInput').value;
-    const inputTensor = tf.tensor([parseInt(userInput)]).reshape([1, 1]);  
-
-
-    // This is the way
-    const modelParameters = ['lPokoi', 'powierzchnia_corr', 'powierzchniaDzialki_corr', 'rokBudowy_corr', 'cena/m', 'lPieter_crr', 'locationX', 'locationY', 'rodzajZabudowy_0', 'rodzajZabudowy_bliźniak', 'rodzajZabudowy_dworek/pałac', 'rodzajZabudowy_gospodarstwo', 'rodzajZabudowy_kamienica', 'rodzajZabudowy_szeregowiec', 'rodzajZabudowy_wolnostojący', 'materialBudynku_0', 'materialBudynku_beton', 'materialBudynku_beton_komórkowy', 'materialBudynku_cegła', 'materialBudynku_drewno', 'materialBudynku_inne', 'materialBudynku_keramzyt', 'materialBudynku_pustak', 'materialBudynku_silikat', 'stanWykonczenia_0', 'stanWykonczenia_do_remontu', 'stanWykonczenia_do_wykończenia', 'stanWykonczenia_do_zamieszkania', 'stanWykonczenia_stan_surowy_otwarty', 'stanWykonczenia_stan_surowy_zamknięty', 'okna_0', 'okna_aluminiowe', 'okna_brak', 'okna_drewniane', 'okna_plastikowe', 'rynek_pierwotny', 'rynek_wtórny'];
+    const modelParameters = ['lPokoi', 'powierzchnia_corr', 'powierzchniaDzialki_corr', 'rokBudowy_corr', 'lPieter_crr', 'locationX', 'locationY', 'rodzajZabudowy_0', 'rodzajZabudowy_bliźniak', 'rodzajZabudowy_dworek/pałac', 'rodzajZabudowy_gospodarstwo', 'rodzajZabudowy_kamienica', 'rodzajZabudowy_szeregowiec', 'rodzajZabudowy_wolnostojący', 'materialBudynku_0', 'materialBudynku_beton', 'materialBudynku_beton_komórkowy', 'materialBudynku_cegła', 'materialBudynku_drewno', 'materialBudynku_inne', 'materialBudynku_keramzyt', 'materialBudynku_pustak', 'materialBudynku_silikat', 'stanWykonczenia_0', 'stanWykonczenia_do_remontu', 'stanWykonczenia_do_wykończenia', 'stanWykonczenia_do_zamieszkania', 'stanWykonczenia_stan_surowy_otwarty', 'stanWykonczenia_stan_surowy_zamknięty', 'okna_0', 'okna_aluminiowe', 'okna_brak', 'okna_drewniane', 'okna_plastikowe', 'rynek_pierwotny', 'rynek_wtórny'];
+ 
     inputsTensor = tf.tensor([]);
+ 
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
     modelParameters.forEach(
         (element, idx) => {
@@ -47,19 +55,35 @@ function getUserInput() {
         }
             
     )
-    inputsTensor.print();
     // console.log(modelParameters.length, inputsTensor.shape);
 
+    // Normalize user input parameters
+    inputsTensor = normalizeUserInput(inputsTensor);
 
-
-    return inputTensor    
+    return inputsTensor    
 };
+
+
+// Normalize user input parameters
+async function normalizeUserInput(inputsTensor) {
+    const {means, variances} = await fetchNormalizationParameters_new();
+    // console.log('means: ', means.shape);
+    // console.log('variances:', variances.shape);
+    // console.log('inputsTensor:', inputsTensor.shape);
+    var inputsTensorNormalized = inputsTensor.sub(means).div(variances);
+    inputsTensorNormalized.print();
+    return inputsTensorNormalized
+}
+
+
 
     
 // Predict with model loaded from file
 async function predict(loadedModel) {
     // take User input and normalize it
-    const inputTensor = getUserInput();
+    const inputsTensor = getUserInput();
+    const userInput = document.getElementById('userInput').value;
+    const inputTensor = tf.tensor([parseInt(userInput)]).reshape([1, 1]);  
 
     // normalize user input
     const normalizationParameters = await fetchNormalizationParameters();
